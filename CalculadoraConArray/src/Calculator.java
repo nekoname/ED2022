e068f248b33f29bd7c60eb7632e6f68ff0e55e68
nekoname <ltorrente@nigul.cide.es>
2021-11-10 20:02:41 +0100

    j

--- CalculadoraConArray/src/Calculator.java
+++ CalculadoraConArray/src/Calculator.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         double resultado[] = new double[10];
-        double resultadoFinal[];
-        for (int i = 0; i < 10; i++) {
+        for (int i=0; i < 10;i++) {
             System.out.println("Selección de operación: 1.Suma 2.Resta 3.Multiplicación 4.División");
             Scanner scan = new Scanner(System.in);
             int election = scan.nextInt();
@@ -17,35 +16,41 @@ public class Calculator {
             double operador1 = scan.nextDouble();
             scan = new Scanner(System.in);
             double operador2 = scan.nextDouble();
-            for (int x = 0; x < 10; x++)
-                switch (election) {
-                    //en cada caso se hará una llamada al método calculador con los argumento:Tipo operación, operador1 y operador 2
-                    case 1:
-                        resultado[posicionArray] = o1 + o2;
-                        break;
-                    case 2:
-                        resultado[posicionArray] = o1 - o2;
-                        break;
-                    case 3:
-                        resultado[posicionArray] = o1 * o2;
-                        break;
-                    case 4:
-                        if (o2 != 0) {
-                            resultado[posicionArray] = o1 / o2;
-                        } else {
-                            System.out.println("Estás jugando a ser Dios");
-                        }
-                        break;
-                    default:
-                        System.out.println("Opción no contemplada");
+            double resultadoFinal[] = calcular(election,operador1,operador2, resultado,i);
 
-                        System.out.println(x + " " + resultado[x]);
-                }
+            for (int x=0;x < 10;x++)
+                System.out.println(x + " " + resultadoFinal[x]);
+        }
+    }
+        public static double[] calcular(int tipoOperacion, double o1, double o2, double []resultado,int i) {
+            if (resultado[i] != resultado[0])
+                o1 = resultado[i];
+            switch (tipoOperacion) {
+                //en cada caso se hará una llamada al método calculador con los argumento:Tipo operación, operador1 y operador 2
+                case 1:
+                    resultado[i] = o1 + o2;
+                    break;
+                case 2:
+                    resultado[i] = o1 - o2;
+                    break;
+                case 3:
+                    resultado[i] = o1 * o2;
+                    break;
+                case 4:
+                    if (o2 != 0) {
+                        resultado[i] = o1 / o2;
+                    } else {
+                        System.out.println("Estás jugando a ser Dios");
+                    }
+                    break;
+                default:
+                    System.out.println("Opción no contemplada");
 
+            }
+            return resultado;
         }
 
 
-    }}
-
+    }
 
 
